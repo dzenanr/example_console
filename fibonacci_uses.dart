614a33f9c95b6c1b724502aee7eedbf5e43d306e
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-02-14 11:27:22 -0500

    update output text

--- fibonacci_uses.dart
+++ fibonacci_uses.dart
@@ -1,32 +1,32 @@
 part of example_console;
 
 fibonacci1(DartConsole dartConsole) {
-  int count = 10;
+  int count = 11;
   String result = fibonacciIteration(count);
-  dartConsole.sline('Fibonacci with iteration: $count');
+  dartConsole.sline('Fibonacci with iteration: sequence length ${count + 1} (including 0)');
   dartConsole.line(result);
 }
 
 fibonacci2(DartConsole dartConsole) {
-  int count = 10;
-  int result = fibonacciRecursion(count);
-  dartConsole.sline('Fibonacci with recursion: $count');
-  dartConsole.line('$result');
+  int position = 11;
+  int positionNumber = fibonacciRecursion(position);
+  dartConsole.sline('Fibonacci with recursion: position $position (positions start with 0)');
+  dartConsole.line('Position number: $positionNumber');
 }
 
 fibonacci3(DartConsole dartConsole) {
   var map = new Map();
   map[0] = 0;
   map[1] = 1;
-  int count = 10;
+  int count = 11;
   fibonacciRecursionMap(count, map);
-  dartConsole.sline('Fibonacci with recursion and map: $count');
+  dartConsole.sline('Fibonacci with recursion and map: sequence length ${count + 1} (including 0)');
   map.forEach((k,v) => dartConsole.line('$k: $v'));
 }
 
 fibonacci4(DartConsole dartConsole) {
-  int count = 10;
+  int count = 11;
   var map = fibonacciRecursionMapClosure(count);
-  dartConsole.sline('Fibonacci with recursion, map and closure: $count');
+  dartConsole.sline('Fibonacci with recursion, map and closure: sequence length ${count + 1} (including 0)');
   map.forEach((k,v) => dartConsole.line('$k: $v'));
 }
